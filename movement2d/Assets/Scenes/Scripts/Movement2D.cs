d8579c00f8bd8f3f96b9cfc4684ef7a42478425d
CHOIJIONE\choij <choijione02@gmail.com>
2025-06-30 07:50:06 +0900

    방향키 (or WASD)에 따라 플레이어 이동을 제어하는 기능 추가

--- movement2d/Assets/Scenes/Scripts/Movement2D.cs
+++ movement2d/Assets/Scenes/Scripts/Movement2D.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 public class Movement2D : MonoBehaviour
 {
+    private float moveSpeed = 5.0f;
+    private Vector3 moveDirection = Vector3.zero;
+
     private void Update()
     {
-        transform.position += Vector3.right * Time.deltaTime;
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector3(x, y, 0.0f);
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
     }
 }
